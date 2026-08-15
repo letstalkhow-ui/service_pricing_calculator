f5e3abfeeb54979c1fc2912d5b4c47702cccdea1
letstalkhow-ui <letstalkhow@gmail.com>
2026-08-15 18:02:21 +0400

    Bust Cost Estimator polish cache

--- api/cost-estimator-page.js
+++ api/cost-estimator-page.js
@@ -5,9 +5,11 @@ module.exports = function handler(req, res) {
   try {
     const filePath = path.join(process.cwd(), 'tools', 'cost-estimator.html');
     let html = fs.readFileSync(filePath, 'utf8');
-    html = html.replace('</body>', '<script src="/cost-estimator-polish.js"></script></body>');
+    html = html.replace('</body>', '<script src="/cost-estimator-polish.js?v=d95689235e4693828e52f835268e1a707fffd2e3"></script></body>');
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.setHeader('Cache-Control', 'no-store');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
     res.status(200).send(html);
   } catch (error) {
     console.error('Cost estimator page error', error);
